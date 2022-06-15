8ea23a1d87f9c6d062eb5f5d7a408607bdb99189
Souptik Datta <souptikdatta2001@gmail.com>
2022-06-16 02:01:20 +0530

    fix: Fix parsely script loading issues

--- packages/analytics-plugin-parsely/src/browser.js
+++ packages/analytics-plugin-parsely/src/browser.js
@@ -15,36 +15,36 @@
  *   }
  * })
  */
- function parselyPlugin(pluginConfig = {}) {
-	// Parsely client instance.
-	let client = null;
-	// Flag is set to true after parsely client instance is initialized.
-	let parselyInitCompleted = false;
+function parselyPlugin(pluginConfig = {}) {
+// Parsely client instance.
+let client = null;
+// Flag is set to true after parsely client instance is initialized.
+let parselyInitCompleted = false;
 
-	const initComplete = (d) => {
-	  client = d;
-	  parselyInitCompleted = true;
-	};
+const initComplete = (d) => {
+	client = d;
+	parselyInitCompleted = true;
+};
 
-	return {
-	  name: "parsely",
-	  config: pluginConfig,
-	  initialize: ({ config }) => {
+return {
+	name: "parsely",
+	config: pluginConfig,
+	initialize: ({ config }) => {
 		const { apiKey, options = {} } = config
 		if (!apiKey) {
-		  throw new Error("Parsely project API key is not defined")
+			throw new Error("Parsely project API key is not defined")
 		}
 		if (options && typeof options !== "object") {
-		  throw new Error("Parsely SDK options must be an object")
+			throw new Error("Parsely SDK options must be an object")
 		}
 
-		// Already loaded
+		// Already loaded.
 		if (typeof window.PARSELY !== 'undefined') {
-		  return
+			return;
 		}
 
-		// Initialize parsely script
-		(function(){
+		// Initialize parsely script.
+		window.addEventListener('load', () => {
 			var parsely=window.PARSELY=window.PARSELY||{
 				autotrack: false,
 				onload: function() {
@@ -53,64 +53,60 @@
 				}
 			};
 			if(parsely._snippetVersion="1.0.0",!parsely.loaded)if(parsely._snippetInvoked)try{window.console&&console.error&&console.error("Parsely snippet included twice")}catch(e){}else{parsely._snippetInvoked=!0,parsely._stubs={onStart:[]},parsely._buildStub=function(e){return function(){parsely._stubs[e].push(arguments)}};for(var curStub in parsely._stubs)parsely._stubs.hasOwnProperty(curStub)&&(parsely[curStub]=parsely._buildStub(curStub))}parsely._load=function(e,s){s=void 0===s?"cdn.parsely.com":s;var r=document.createElement("script");r.id="parsely-cfg",r.type="text/javascript",r.async=!0,r.setAttribute("data-parsely-site",e),r.src="//"+s+"/keys/"+e+"/p.js",document.body.appendChild(r)};
-			// Parse.ly users: put your site id (aka apikey) in the call to _load() here (example: parsely._load("mysite.com"))
-			// Create the script tags.
+
 			parsely._load(apiKey);
-			})();
+		})
+	},
 
-		// Set initial session id. Ref https://bit.ly/3vElAym
-		if (initialSessionId) {
-		  setTimeout(() => setSessionId(initialSessionId), 10)
-		}
-	  },
+	page: ({ payload: { properties, options } }) => {
+	let eventType = "Page View"
+	if (options && options.eventType) {
+		eventType = options.eventType
+	}
+		client.logEvent(eventType, properties)
+	},
 
-	  page: ({ payload: { properties, options } }) => {
-		let eventType = "Page View"
-		if (options && options.eventType) {
-		  eventType = options.eventType
-		}
-		 client.logEvent(eventType, properties)
-	  },
+	track: ({ payload: { event, properties } }) => {
+		client.beacon.trackPageView({
+		url: "target url",
+		urlref: location.href, // current url
+		js: 1,
+		action: "_" . event, // the event string which is passed
+		data: properties // payload
+	});
+	},
 
-	  track: ({ payload: { event, properties } }) => {
-		 client.beacon.trackPageView({
-			url: "target url",
-			urlref: location.href, // current url
-			js: 1,
-			action: "_" . event, // the event string which is passed
-			data: properties // payload
-		});
-	  },
-
-	  identify: ({ payload: { apiKey, userId, traits }, instance }) => {
-		fetch(`https://api.parsely.com/v2/profile?apikey=${apiKey}&url=${window.location.href}&uuid=${userId}`)
+	identify: ({payload}) => {
+		let {apiKey, userId} = payload.traits;
+		fetch(`https://api.parsely.com/v2/profile?apikey=${apiKey}&url=${window.location.href}}&uuid=${userId}`)
 		.then(response => response.json())
 		.then(data => data)
 		.catch((data) => {
 			console.log("Failure: ");
 			console.log(data);
 		})
-	  },
+	},
 
-	  user: ({payload: {apiKey, requiredData}}) => {
-		switch (requiredData) {
-			case 'history':
-				fetch(`https://api.parsely.com/v2/history?apikey=${apiKey}&uuid=${userId}`)
-				.then(response => response.json())
-				.then(data => data)
-				.catch((data) => {
-					console.log("Failure: ");
-					console.log(data);
-					return data;
-				})
-				break;
-			default:
-				return "No such data available";
-		}
-	  },
-
-	  loaded: () => parselyInitCompleted,
+	user: ({payload}) => {
+	let {apiKey, requiredData} = payload;
+	switch (requiredData) {
+		case 'history':
+			fetch(`https://api.parsely.com/v2/history?apikey=${apiKey}&uuid=${userId}`)
+			.then(response => response.json())
+			.then(data => data)
+			.catch((data) => {
+				console.log("Failure: ");
+				console.log(data);
+				return data;
+			})
+			break;
+		default:
+			return "No such data available";
 	}
-  }
+	},
 
-  export default parselyPlugin
+	loaded: () => parselyInitCompleted,
+}
+}
+
+export default parselyPlugin;
